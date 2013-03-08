187871d1a55b5bc7dac981d28f7009045abcd046
Mark Wheeler <mark@markwheeler.net>
2013-03-07 20:37:36 -0800

    Added trails

--- Sequencer/EatsGridIntroView.m
+++ Sequencer/EatsGridIntroView.m
@@ -11,11 +11,15 @@
 @interface EatsGridIntroView ()
 
 #define FRAMERATE 60
-#define TRAIL_LENGTH 2
+#define TRAIL_LENGTH 4 // Won't show past 15
+#define PAUSE_AT_END 10
 
 @property NSTimer               *animationTimer;
 @property uint                  currentFrame;
+
 @property NSArray               *okArray;
+@property long                  okLeftMargin;
+@property long                  okTopMargin;
 @property NSMutableDictionary   *particleA;
 @property NSMutableDictionary   *particleB;
 @property NSMutableArray        *particleATrail;
@@ -38,16 +42,28 @@
         
         self.currentFrame = 0;
         
-        self.particleA = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInt:0], @"x", [NSNumber numberWithUnsignedInt:(self.height / 2) - 1], @"y", nil];
-        self.particleB = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInt:0], @"x", [NSNumber numberWithUnsignedInt:(self.height / 2)], @"y", nil];
-        
+        // Generate the OK
         NSNumber *y = [NSNumber numberWithUnsignedInt:15];;
         NSNumber *n = [NSNumber numberWithUnsignedInt:0];
-        self.okArray = [NSArray arrayWithObjects:[NSArray arrayWithObjects: y, y, y, n, n, y, n, y, nil],
-                                                 [NSArray arrayWithObjects: y, n, y, n, n, y, y, n, nil],
-                                                 [NSArray arrayWithObjects: y, n, y, n, n, y, n, y, nil],
-                                                 [NSArray arrayWithObjects: y, y, y, n, n, y, n, y, nil],
+        self.okArray = [NSArray arrayWithObjects:[NSArray arrayWithObjects: y, y, y, y, nil],
+                                                 [NSArray arrayWithObjects: y, n, n, y, nil],
+                                                 [NSArray arrayWithObjects: y, y, y, y, nil],
+                                                 [NSArray arrayWithObjects: n, n, n, n, nil],
+                                                 [NSArray arrayWithObjects: n, n, n, n, nil],
+                                                 [NSArray arrayWithObjects: y, y, y, y, nil],
+                                                 [NSArray arrayWithObjects: n, y, n, n, nil],
+                                                 [NSArray arrayWithObjects: y, n, y, y, nil],
                                                  nil];
+        
+        self.okLeftMargin = (self.width - [self.okArray count]) / 2;
+        self.okTopMargin = (self.height - [[self.okArray objectAtIndex:0] count]) / 2;
+        
+        // Set the particle start positions
+        self.particleA = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInt:0], @"x", [NSNumber numberWithUnsignedInt:(self.height / 2) - 1], @"y", nil];
+        self.particleB = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInt:0], @"x", [NSNumber numberWithUnsignedInt:(self.height / 2)], @"y", nil];
+        self.particleATrail = [NSMutableArray arrayWithCapacity:TRAIL_LENGTH];
+        self.particleBTrail = [NSMutableArray arrayWithCapacity:TRAIL_LENGTH];
+        
         [self startAnimation];
         
     }
@@ -60,20 +76,15 @@
 
 - (void) updateAnimation:(NSTimer *)sender
 {
-    NSMutableArray *gridArray = [NSMutableArray array];
-    
-    long leftMargin = (self.width - [[self.okArray objectAtIndex:0] count]) / 2;
-    long topMargin = (self.height - [self.okArray count]) / 2;
-    
-    // Generate the columns
+    // Generate the array
+    NSMutableArray *gridArray = [NSMutableArray arrayWithCapacity:self.height * self.width];
     for(uint x = 0; x < self.width; x++) {
         [gridArray insertObject:[NSMutableArray arrayWithCapacity:self.height] atIndex:x];
-        // Generate the rows
         for(uint y = 0; y < self.height; y++) {
             // Put OK in
-            if(x > leftMargin - 1 && x <= self.width - leftMargin - 1 && y > topMargin - 1 && y <= self.height - topMargin - 1) {
+            if(x >= self.okLeftMargin && x < self.okLeftMargin + [self.okArray count] && y >= self.okTopMargin && y < self.okTopMargin + [[self.okArray objectAtIndex:0] count]) {
                 if(!self.particleA || x <= [[self.particleA valueForKey:@"x"] unsignedIntValue])
-                    [[gridArray objectAtIndex:x] insertObject:[[self.okArray objectAtIndex:y - topMargin] objectAtIndex:x - leftMargin] atIndex:y];
+                    [[gridArray objectAtIndex:x] insertObject:[[self.okArray objectAtIndex:x - self.okLeftMargin] objectAtIndex:y - self.okTopMargin] atIndex:y];
                 else
                     [[gridArray objectAtIndex:x] insertObject:[NSNumber numberWithUnsignedInt:0] atIndex:y];
             } else {
@@ -86,21 +97,34 @@
     if(self.particleA)
         [[gridArray objectAtIndex:[[self.particleA valueForKey:@"x"] unsignedIntValue]] replaceObjectAtIndex:[[self.particleA valueForKey:@"y"] unsignedIntValue]
                                                                                                   withObject:[NSNumber numberWithUnsignedInt:15]];
-    
-    
     // Add particle B
     if(self.particleB)
         [[gridArray objectAtIndex:[[self.particleB valueForKey:@"x"] unsignedIntValue]] replaceObjectAtIndex:[[self.particleB valueForKey:@"y"] unsignedIntValue]
                                                                                                   withObject:[NSNumber numberWithUnsignedInt:15]];
     
+    // Draw trails
+    for(uint i = 0; i < [self.particleATrail count]; i++) {
+        uint endFix = 0;
+        if(!self.particleA)
+            endFix = TRAIL_LENGTH - (uint)[self.particleATrail count];
+        [[gridArray objectAtIndex:[[[self.particleATrail objectAtIndex:i] valueForKey:@"x"] unsignedIntValue]] replaceObjectAtIndex:[[[self.particleATrail objectAtIndex:i] valueForKey:@"y"] unsignedIntValue] withObject:[NSNumber numberWithUnsignedInt:15 - i - endFix]];
+        [[gridArray objectAtIndex:[[[self.particleBTrail objectAtIndex:i] valueForKey:@"x"] unsignedIntValue]] replaceObjectAtIndex:[[[self.particleBTrail objectAtIndex:i] valueForKey:@"y"] unsignedIntValue] withObject:[NSNumber numberWithUnsignedInt:15 - i - endFix]];
+    }
+    
     // Update via delegate
     if([self.delegate respondsToSelector:@selector(updateGridWithArray:)])
         [self.delegate performSelector:@selector(updateGridWithArray:) withObject:gridArray];
     
+    // Advance the frame counter
     self.currentFrame++;
     
-    for(int i = 0; i < TRAIL_LENGTH; i++) {
-        // save past positions of particles?
+    // Save trail info
+    if(self.particleA) [self.particleATrail insertObject:[self.particleA copy] atIndex:0];
+    if(self.particleB) [self.particleBTrail insertObject:[self.particleB copy] atIndex:0];
+    
+    if([self.particleATrail count] > TRAIL_LENGTH || (!self.particleA && [self.particleATrail count] > 0) ) {
+        [self.particleATrail removeLastObject];
+        [self.particleBTrail removeLastObject];
     }
     
     // Move the particles
@@ -118,7 +142,7 @@
         self.particleB = nil;
     }
     
-    if (self.currentFrame > self.width + self.height - 2 + 10) [self stopAnimation];
+    if (self.currentFrame > self.width + self.height - 2 + TRAIL_LENGTH + PAUSE_AT_END) [self stopAnimation];
 }
 
 - (void) startAnimation
