43950ffc5d5d0f02ac8842a285cab2ecca3a8a04
Mark Wheeler <mark@markwheeler.net>
2013-04-22 21:31:34 +0100

    Revert "This version removes several fetch requests that could be avoided but still crashes badly"
    This reverts commit 88201761c3f68fd16a0bbc6529c68ed4dd5c30ef.

--- Sequencer/EatsGridPlayViewController.m
+++ Sequencer/EatsGridPlayViewController.m
@@ -60,13 +60,13 @@
 
 - (void) setupView
 {
-    if( ![NSThread isMainThread] ) NSLog(@"%s is NOT running on main thread", __func__);
-    
     // Get the sequencer
-    _sequencer = [self.delegate valueForKey:@"sequencer"];
+    NSFetchRequest *sequencerRequest = [NSFetchRequest fetchRequestWithEntityName:@"Sequencer"];
+    NSArray *sequencerMatches = [self.managedObjectContext executeFetchRequest:sequencerRequest error:nil];
+    _sequencer = [sequencerMatches lastObject];
     
     // Get the pattern
-    _pattern = [self.delegate valueForKey:@"currentPattern"];
+    _pattern = [self.delegate valueForKey:@"pattern"];
     
     // Get prefs
     _sharedPreferences = [Preferences sharedPreferences];
@@ -213,27 +213,20 @@
 
 - (void) updateView
 {
-    
-    if( ![NSThread isMainThread] ) NSLog(@"%s is NOT running on main thread", __func__);
-    
-    if( _pattern != [self.delegate valueForKey:@"currentPattern"] )
-        _pattern = [self.delegate valueForKey:@"currentPattern"];
+    if( _pattern != [self.delegate valueForKey:@"pattern"] )
+        _pattern = [self.delegate valueForKey:@"pattern"];
     
     // Update PatternView sub view
-    NSError *requestError = nil;
     NSFetchRequest *patternRequest = [NSFetchRequest fetchRequestWithEntityName:@"SequencerPattern"];
     patternRequest.predicate = [NSPredicate predicateWithFormat:@"SELF == %@", _pattern];
-    NSArray *patternMatches = [self.managedObjectContext executeFetchRequest:patternRequest error:&requestError];
-    
-    if( requestError )
-        NSLog(@"Request error: %@", requestError);
+    NSArray *patternMatches = [self.managedObjectContext executeFetchRequest:patternRequest error:nil];
     
     _pattern = [patternMatches lastObject];
     
     _patternView.pattern = _pattern;
     
     // Start or stop flashing nextPatternId
-    NSNumber *nextPatternId = _pattern.inPage.nextPatternId;
+    NSNumber *nextPatternId = [_pattern.inPage.nextPatternId copy];
     if( nextPatternId == nil && _flashingNextPatternTimer ) {
         [self stopNextPatternFlashing];
         
@@ -248,13 +241,12 @@
     NSNumber *nextStep = _pattern.inPage.nextStep;
     if( nextStep == nil && _flashingNextStepTimer ) {
         [self stopNextStepFlashing];
-        
     } else if( nextStep != _currentlyFlashingStep ) {
         [self stopNextStepFlashing];
         [self startNextStepFlashing];
     }
     _currentlyFlashingStep = nextStep;
-    
+        
     // Set buttons etc
     [self setPlayMode:_pattern.inPage.playMode.intValue];
     [self setActivePageButton];
